b732b980a8d62892c6007aa88dc92988a4a455c1
josemarsalvador7 <josemarsalvador2027@gmail.com>
2026-07-13 11:51:06 +0100

    adicionado accounts app

--- acconts/admin.py
+++ acconts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from acconts.models import Profile
+
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['avatar','code','role']
+    search_fields = ['code','role']
